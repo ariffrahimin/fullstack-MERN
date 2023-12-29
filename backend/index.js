fe1140e107c161f57a8d5038355cc3e303539c81
Ariff Kamski <ariffrahimin99@gmail.com>
2023-12-30 00:36:44 +0800

    FEAT: connect mongodb using mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,17 @@
 import express from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
 
-const app = erxpress();
+const app = express();
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("App connected to the database");
+    app.listen(PORT, () => {
+      console.log(`App is listening to port:${PORT} `);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
