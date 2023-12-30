89150ddacebaaec1c576a71fabcd36f0bf1c4fbd
Ariff Kamski <ariffrahimin99@gmail.com>
2023-12-30 12:34:03 +0800

    FEAT: create a post request

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,33 @@
 import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
 
 const app = express();
 
+// route to save a new book
+app.post("/books", async (req, res) => {
+  try {
+    if (!req.body.title || !req.body.author || !req.body.publishYear) {
+      return res.status(400).send({
+        message: "Send all required fields: title, author, publishYear",
+      });
+    }
+    const newBook = {
+      title: req.body.title,
+      author: req.body.author,
+      publishYear: req.body.publishYear,
+    };
+    const book = await Book.create(newBook);
+
+    return res.status(201).send(book);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: error.message });
+  }
+});
+
+//connect to the database
 mongoose
   .connect(mongoDBURL)
   .then(() => {
